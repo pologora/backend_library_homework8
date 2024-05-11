6722466495eda74b1a1285d9741c747756f8e21a
Oleksandr <lysakova@yahoo.com>
2024-05-11 14:47:30 +0200

    refactor: change book class model methods names to more general ones

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -3,14 +3,14 @@ import { Book } from '../models/Book';
 import { withErrorHandling } from '../middleware/withErrorHandling';
 
 export const getAllbooks = withErrorHandling(async (req: Request, res: Response, next: NextFunction) => {
-  const result = await Book.getAllbooks();
+  const result = await Book.getAll();
 
   res.status(200).json({ status: 'success', data: result });
 });
 
 export const getBookById = withErrorHandling(async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
-  const result = await Book.getBook(Number(id));
+  const result = await Book.getOne(Number(id));
 
   res.status(200).json({ status: 'success', data: result });
 });
@@ -18,7 +18,7 @@ export const getBookById = withErrorHandling(async (req: Request, res: Response,
 export const createNewBook = withErrorHandling(async (req: Request, res: Response, next: NextFunction) => {
   const { price, author, quantity, title }: Book = req.body;
   const book = new Book({ price, author, quantity, title });
-  await Book.createBook(book);
+  await Book.createOne(book);
 
   res.status(201).json({ status: 'success' });
 });
@@ -27,13 +27,13 @@ export const updateBook = withErrorHandling(async (req: Request, res: Response, 
   const { id } = req.params;
   const { price, author, quantity, title } = req.body;
 
-  await Book.updateBook(Number(id), { price, author, quantity, title });
+  await Book.updateOne(Number(id), { price, author, quantity, title });
   res.status(204).end();
 });
 
 export const deleteBook = withErrorHandling(async (req: Request, res: Response, next: NextFunction) => {
   const { id } = req.params;
-  await Book.deleteBook(Number(id));
+  await Book.deleteOne(Number(id));
 
   res.status(204).end();
 });
