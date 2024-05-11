7c8dadcab647685b4e79395ed9140a39c4654b61
Oleksandr <lysakova@yahoo.com>
2024-05-11 14:48:12 +0200

    feat: implement diff messages for errors in production and development

--- src/controllers/errorController.ts
+++ src/controllers/errorController.ts
@@ -1,9 +1,28 @@
 import { NextFunction, Request, Response } from 'express';
 import { AppError } from '../utils/AppError';
 
-export const globalErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
+const sendErrDevelopment = (err: AppError, res: Response) => {
   const statusCode = err.statusCode || 500;
   const status = err.status || 'error';
+  return res.status(statusCode).json({ status, error: err.message, message: err.message, stack: err.stack });
+};
 
-  return res.status(statusCode).json({ status, error: err.message });
+const sendErrProduction = (err: AppError, res: Response) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || 'error';
+  const message = err.isOperational ? err.message : 'Somthing goes wrong, please try again later';
+
+  return res.status(statusCode).json({ status, error: message });
+};
+
+export const globalErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
+  if (!err.isOperational) {
+    console.log(err);
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrDevelopment(err, res);
+  } else {
+    sendErrProduction(err, res);
+  }
 };
